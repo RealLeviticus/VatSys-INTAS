04f1e55e9bcb643cd34ea891a08122bd1df84b67
Levi Shaw <levishaw@outlook.com>
2023-02-12 22:31:19 +1100

    Added StripHolder Component

---
+++ Components/StripHolder.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace vatSys_INTAS_Client.Components
+{
+    /// <summary>
+    /// Interaction logic for FDRHolder.xaml
+    /// </summary>
+    public partial class StripHolder : UserControl
+    {
+        public StripHolder()
+        {
+            InitializeComponent();
+        }
+
+        public string Description
+        {
+            get { return (string)GetValue(DescriptionProperty); }
+            set { SetValue(DescriptionProperty, value); }
+        }
+
+        public static readonly DependencyProperty DescriptionProperty =
+            DependencyProperty.Register("Description", typeof(string), typeof(StripHolder), new PropertyMetadata(string.Empty));
+
+
+    }
+}
